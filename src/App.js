848f4c83ba73fa93596dca1efdaabc519c1b8001
Sarabjeet Singh <sarabjeet@ucraete.co.in>
2019-11-22 11:41:09 +0530

    update site title

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Everyone.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.meetup.com/DevTalks-India/"
           target="_blank"
           rel="noopener noreferrer"
         >
