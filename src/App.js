ef144c4190d012b15a4767a854afc6dfbe12f1a3
Sarabjeet Singh <sarabsingh212@gmail.com>
2020-05-26 22:58:54 +0530

    Implement API call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import logo from './logo.jpeg';
 import Section from "./components/Section/Section";
@@ -7,14 +7,16 @@ import GridItem from "./components/GridItem/GridItem";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
+  useEffect(() => {
+    fetch('https://devtalks-events-ad2nk4diqq-ew.a.run.app/events')
+      .then(data => data.json())
+      .then(data => console.log('data', data));
+  }, []);
   return (
     <div className="App">
       <Navigation />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/*<p>
-          Hello Everyone. <span aria-label="wave" role="img">👋</span>
-        </p>*/}
         <div className="grid">
           <div className="subscribeYoutube">
             <p>Due to COVID-19 outbreak we have moved all of our meetups online.<br /> Please subscribe to our YouTube channel for updates and sessions</p>
