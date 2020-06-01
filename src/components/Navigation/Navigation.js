e5695c0690378d2338b2a6220f8bca78ef7289e6
Sarabjeet Singh <sarabsingh212@gmail.com>
2020-06-01 23:23:44 +0530

    extend navigation with logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './navigation.css';
+import logo from "../../logo.jpeg";
 
-const Navigation = () => {
+const Navigation = ({ withLogo }) => {
+  const navClass = withLogo ? 'navigation navigation-with-logo' : 'navigation';
   return (
-      <div className="navigation">
+      <div className={navClass}>
         <div className="navigation-small">
           <Link className="link" to="/home">Home</Link>
           <Link className="link" to="/events">Events</Link>
         </div>
+        {withLogo && <div className="navigation-small">
+          <img width={192} src={logo}  alt="logo" />
+        </div>}
         <div className="navigation-small">
-          <Link className="link" to="/speaker">Speakers</Link>
+          <Link className="link" to="/speakers">Speakers</Link>
           <Link className="link" to="/contact">Contact</Link>
         </div>
 
       </div>
   );
 };
-
+Navigation.propTypes = {
+  withLogo: PropTypes.bool,
+};
+Navigation.defaultProps = {
+  withLogo: false,
+};
 export default Navigation;
