0e4aafbab2c471ace9e7db12d4be3284157804fc
Sarabjeet Singh <sarabsingh212@gmail.com>
2020-06-01 23:23:54 +0530

    new speakers page

---
+++ src/pages/Speakers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from "../components/Navigation/Navigation";
+
+const Speakers = () => {
+  return (
+      <div>
+        <Navigation withLogo />
+        Speakers
+      </div>
+  );
+};
+
+export default Speakers;
