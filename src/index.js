a89d24127ed0229e655da5c165b6455f4971301a
Sarabjeet Singh <sarabsingh212@gmail.com>
2020-06-01 23:23:16 +0530

    fix error

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Speakers from "./pages/Speakers";
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 ReactDOM.render(
+    <div style={{ position: 'absolute', top: 0, width: '100%' }}>
     <Router>
-      <Switch>
-        <Route component={App} path="/" />
-      </Switch>
-    </Router>, document.getElementById('root'));
+      <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition
+              key={location.key}
+              timeout={300}
+              classNames='fade'
+          >
+            <Switch location={location}>
+              <Route component={Speakers} path="/Speakers" />
+              <Route component={App} path="/" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
+    </Router>
+    </div>, document.getElementById('root'));
